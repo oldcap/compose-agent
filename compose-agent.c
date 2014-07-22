17944889867370ed645cd888c565f1b6346e4c61
oldcap <zhe.zhang.research@gmail.com>
2014-07-22 15:00:25 -0400

    New agent

--- compose-agent.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-int main(int argc, char **argv)
-{
-	while (1) {
-		;
-	}
-	return 0;
-}
---
+++ compose-agent.cpp
@@ -0,0 +1,32 @@
+#include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int read_config(string fname, unordered_map <string, int> *m)
+{
+	ifstream config_file (fname);
+	string line;
+	if (config_file.is_open()) {
+		while (getline(config_file,line)) {
+			cout << line << '\n';
+		}
+
+		config_file.close();
+	}
+	// (*m)["foo"] = 77;
+	return 0;
+}
+int main(int argc, char **argv)
+{
+	unordered_map <string, int> map;
+	read_config("compose-agent.cpp", &map);
+
+	cout << map["foo"] << endl;
+	while (1) {
+		;
+	}
+	return 0;
+}
